7f94da61fb3100064310525c67873f033e3b3e6a
SirMengo <msimoes0103@gmail.com>
2025-04-04 12:10:39 +0100

    Removed main of ex06

--- ex06/ft_print_alphabet.c
+++ ex06/ft_print_alphabet.c
@@ -6,25 +6,20 @@
 /*   By: msimoes <msimoes@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 12:00:26 by msimoes           #+#    #+#             */
-/*   Updated: 2025/04/04 12:05:02 by msimoes          ###   ########.fr       */
+/*   Updated: 2025/04/04 12:09:56 by msimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_print_aplhaber(void)
+void	ft_print_aplhaber(void)
 {
-	char c;
-	
+	char	c;
+
 	c = 'a';
 	while (c <= 'z')
 	{
 		write(1, &c, 1);
 		c++;
 	}
-}
-
-int main()
-{
-	ft_print_aplhaber();	
 }
