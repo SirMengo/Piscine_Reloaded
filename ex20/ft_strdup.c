29223ffaa36f8ae794675c4495eab7b44b0ca823
SirMengo <msimoes0103@gmail.com>
2025-04-05 22:49:34 +0100

    Completed ex20

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msimoes <msimoes@student.42lisboa.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/05 22:27:33 by msimoes           #+#    #+#             */
+/*   Updated: 2025/04/05 22:48:59 by msimoes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	j = ft_strlen(src);
+	str = malloc(sizeof(char) * j);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		str[i] = src[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
