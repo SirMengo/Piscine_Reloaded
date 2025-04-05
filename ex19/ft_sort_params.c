81a095fdf621f2c8e22783ee40a39053a5b6ab7c
SirMengo <msimoes0103@gmail.com>
2025-04-05 22:32:15 +0100

    Fixed norm errors

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: msimoes <msimoes@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 21:22:07 by msimoes           #+#    #+#             */
-/*   Updated: 2025/04/05 22:20:05 by msimoes          ###   ########.fr       */
+/*   Updated: 2025/04/05 22:31:44 by msimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_putchar(char c);
 
 void	ft_putstr(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
@@ -30,7 +30,7 @@ int	ft_strcmp(char *s1, char *s2)
 {
 	int	i;
 	int	calc;
-	
+
 	i = 0;
 	calc = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
@@ -49,23 +49,23 @@ int	main(int argc, char *argv[])
 	int		count2;
 	char	*temp;
 
-	count1	= 1;
-	count2	= 1;
+	count1 = 1;
+	count2 = 1;
 	while (count1 < argc)
 	{
 		count2 = count1 + 1;
 		while (count2 < argc)
 		{
-			if(ft_strcmp(argv[count1], argv[count2]) > 0)	
+			if (ft_strcmp(argv[count1], argv[count2]) > 0)
 			{
 				temp = argv[count1];
 				argv[count1] = argv[count2];
 				argv[count2] = temp;
-			}	
-			count2++;	
+			}
+			count2++;
 		}
 		count2 = 0;
-		ft_putstr (argv[count1]);
+		ft_putstr(argv[count1]);
 		write(1, "\n", 1);
 		count1++;
 	}
