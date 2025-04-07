39fdcd413f194df79d34ef897617a7bae4d47b05
SirMengo <msimoes0103@gmail.com>
2025-04-07 16:30:59 +0100

    Added edited param on ex19

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -6,13 +6,16 @@
 /*   By: msimoes <msimoes@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 21:22:07 by msimoes           #+#    #+#             */
-/*   Updated: 2025/04/07 14:35:45 by msimoes          ###   ########.fr       */
+/*   Updated: 2025/04/07 16:29:41 by msimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c);
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 void	ft_putstr(char *str)
 {
@@ -33,7 +36,7 @@ int	ft_strcmp(char *s1, char *s2)
 
 	i = 0;
 	calc = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
 		calc = s1[i] - s2[i];
 		i++;
