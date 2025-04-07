76a05370d40349414d304e1c42bc89a44b577f49
SirMengo <msimoes0103@gmail.com>
2025-04-07 16:37:24 +0100

    Removed putchar

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -6,16 +6,13 @@
 /*   By: msimoes <msimoes@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 21:22:07 by msimoes           #+#    #+#             */
-/*   Updated: 2025/04/07 16:29:41 by msimoes          ###   ########.fr       */
+/*   Updated: 2025/04/07 16:35:52 by msimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+void	ft_putchar(char c);
 
 void	ft_putstr(char *str)
 {
